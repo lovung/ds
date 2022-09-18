c10466f60a160e3f88e3c65840518398df08b5ca
lovung <vulong1602@gmail.com>
2022-09-18 17:58:40 +0800

    [Sort] Add quickSort for any types with less function

--- sorts/quick_sort.go
+++ sorts/quick_sort.go
@@ -4,7 +4,7 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-// QuickSort a Slice of interger
+// QuickSort a Slice of ordered types
 func QuickSort[T constraints.Ordered](slice []T) {
 	quickSort(slice, 0, len(slice)-1)
 }
@@ -23,6 +23,33 @@ func partition[T constraints.Ordered](slice []T, lo, hi int) int {
 	i := lo - 1
 	for j := lo; j <= hi-1; j++ {
 		if slice[j] <= pivot {
+			i++
+			slice[i], slice[j] = slice[j], slice[i]
+		}
+	}
+	i++
+	slice[i], slice[hi] = slice[hi], slice[i]
+	return i
+}
+
+// QuickSort a Slice of ordered types
+func QuickSortSlice[T any](slice []T, less func(i, j int) bool) {
+	quickSortSlice(slice, 0, len(slice)-1, less)
+}
+
+func quickSortSlice[T any](slice []T, lo, hi int, less func(i, j int) bool) {
+	if lo >= hi || lo < 0 {
+		return
+	}
+	p := partitionSlice(slice, lo, hi, less)
+	quickSortSlice(slice, lo, p-1, less)
+	quickSortSlice(slice, p+1, hi, less)
+}
+
+func partitionSlice[T any](slice []T, lo, hi int, less func(i, j int) bool) int {
+	i := lo - 1
+	for j := lo; j <= hi-1; j++ {
+		if less(j, hi) {
 			i++
 			slice[i], slice[j] = slice[j], slice[i]
 		}
--- sorts/quick_sort_test.go
+++ sorts/quick_sort_test.go
@@ -11,3 +11,27 @@ func TestQuickSort(t *testing.T) {
 	QuickSort(nums)
 	assert.Equal(t, []int{-3, -1, 0, 2, 4, 5}, nums)
 }
+
+func TestQuickSortSlice(t *testing.T) {
+	type A struct {
+		age int
+	}
+
+	As := []*A{
+		{3},
+		{1},
+		{2},
+		{5},
+		{4},
+	}
+	QuickSortSlice(As, func(i, j int) bool {
+		return As[i].age < As[j].age
+	})
+	assert.Equal(t, []*A{
+		{1},
+		{2},
+		{3},
+		{4},
+		{5},
+	}, As)
+}
