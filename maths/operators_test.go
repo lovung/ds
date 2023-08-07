d8aadb248ea31b230681626616eb199c2cde9bc4
lovung <vulong1602@gmail.com>
2023-08-08 00:30:25 +0800

    [Maths] Add GCD function

--- maths/operators_test.go
+++ maths/operators_test.go
@@ -10,3 +10,10 @@ func TestSum(t *testing.T) {
 	assert.Equal(t, 10, Sum(1, 2, 3, 4))
 	assert.Equal(t, "abcd", Sum("a", "b", "c", "d"))
 }
+
+func TestGCD(t *testing.T) {
+	assert.Equal(t, 10, GCD(10, 20))
+	assert.Equal(t, 5, GCD(15, 20))
+	assert.Equal(t, 15, GCD(15, 0))
+	assert.Equal(t, 4, GCD(12, 16))
+}
